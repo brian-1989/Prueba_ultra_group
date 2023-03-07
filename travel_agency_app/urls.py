72a9b358190e65f6d65ffa19c675f77f82e6ae7e
briayan <briayan.zapata@vivaair.com>
2023-03-07 01:04:37 -0500

    The urls were organized

--- travel_agency_app/urls.py
+++ travel_agency_app/urls.py
@@ -17,15 +17,25 @@ from travel_agency_app.views.room import (
     DeleteRoomView
 )
 
-# Hotel
 urlpatterns = [
+
+]
+
+# Hotel
+urlpatterns += [
     path('get_all_hotels/', GetAllHotelsView.as_view(), ),
     path('create_hotel/', CreateNewHotelView.as_view(), ),
     path('update_hotel/', UpdateHotelView.as_view(), ),
     path('delete_hotel/', DeleteHotelView.as_view(), ),
+]
+
+urlpatterns += [
     path('add_room/', AddRoomView.as_view(), ),
     path('update_room/', UpdateRoomView.as_view(), ),
     path('delete_room/', DeleteRoomView.as_view(), ),
+]
+
+urlpatterns += [
     path('get_hotels_and_rooms/', GetHotelAndRoomView.as_view(), ),
     path('add_booking/', AddBookingView.as_view(), ),
     path('update_booking/', UpdateBookingView.as_view(), ),
