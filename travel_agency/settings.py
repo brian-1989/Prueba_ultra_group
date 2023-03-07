4692f22ca1e5352c8b940a0af1a0feb3d89bab9f
briayan <briayan.zapata@vivaair.com>
2023-03-06 20:17:36 -0500

    Added STATICFILES_STORAGE

--- travel_agency/settings.py
+++ travel_agency/settings.py
@@ -66,6 +66,8 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'travel_agency.urls'
 
 TEMPLATES = [
