ade9c39701f6991828afce5bf50e3ac80dc1f329
briayan <briayan.zapata@vivaair.com>
2023-03-06 21:44:52 -0500

    The DEBUG variable was changed to False

--- travel_agency/settings.py
+++ travel_agency/settings.py
@@ -39,9 +39,11 @@ EMAIL_PASSWORD = config('EMAIL_PASSWORD', cast=str)
 SECRET_KEY = 'django-insecure-#9an^tex%*x6h=$h8^o26*f-3l)f-ky9ql71s=g^4717yb9f+7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOST = ['localhost', '127.0.0.1', '0.0.0.0', 'pruebaultragroup-production.up.railway.app']
+
+print(ALLOWED_HOST)
 
 
 # Application definition
