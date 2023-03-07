b831550da679b200d407dee6d63a761911096bdf
briayan <briayan.zapata@vivaair.com>
2023-03-06 23:56:43 -0500

    Added get function

--- travel_agency_app/views/hotel.py
+++ travel_agency_app/views/hotel.py
@@ -22,6 +22,9 @@ class GetAllHotelsView(APIView):
         return uc.execute()
 
 class CreateNewHotelView(APIView):
+    def get(self, request: Request):
+        return
+
     def post(self, request: Request):
         serializer = CreateNewHotelSerializer(data=request.data)
         serializer.is_valid(raise_exception=True)
