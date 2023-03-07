f2ff4b135a7f369d30df3f13e18907731029747a
briayan <briayan.zapata@vivaair.com>
2023-03-06 23:05:05 -0500

    Added ALLOWED_HOSTS

--- travel_agency/settings.py
+++ travel_agency/settings.py
@@ -39,9 +39,9 @@ EMAIL_PASSWORD = config('EMAIL_PASSWORD', cast=str)
 SECRET_KEY = 'django-insecure-#9an^tex%*x6h=$h8^o26*f-3l)f-ky9ql71s=g^4717yb9f+7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOST = ['*']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
