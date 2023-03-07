927b8c73b1143122e6e0bad1ca1df9439e4ab29e
briayan <briayan.zapata@vivaair.com>
2023-03-07 00:35:45 -0500

    Added changes inn url

--- travel_agency_app/urls.py
+++ travel_agency_app/urls.py
@@ -19,15 +19,15 @@ from travel_agency_app.views.room import (
 
 # Hotel
 urlpatterns = [
-    path('get_all_hotels/', GetAllHotelsView.as_view()),
-    path('create_hotel/', CreateNewHotelView.as_view()),
-    path('update_hotel/', UpdateHotelView.as_view()),
-    path('delete_hotel/', DeleteHotelView.as_view()),
-    path('add_room/', AddRoomView.as_view()),
-    path('update_room/', UpdateRoomView.as_view()),
-    path('delete_room/', DeleteRoomView.as_view()),
-    path('get_hotels_and_rooms/', GetHotelAndRoomView.as_view()),
-    path('add_booking/', AddBookingView.as_view()),
-    path('update_booking/', UpdateBookingView.as_view()),
-    path('delete_booking/', DeleteBookingView.as_view()),
+    path('get_all_hotels/', GetAllHotelsView.as_view(), ),
+    path('create_hotel/', CreateNewHotelView.as_view(), ),
+    path('update_hotel/', UpdateHotelView.as_view(), ),
+    path('delete_hotel/', DeleteHotelView.as_view(), ),
+    path('add_room/', AddRoomView.as_view(), ),
+    path('update_room/', UpdateRoomView.as_view(), ),
+    path('delete_room/', DeleteRoomView.as_view(), ),
+    path('get_hotels_and_rooms/', GetHotelAndRoomView.as_view(), ),
+    path('add_booking/', AddBookingView.as_view(), ),
+    path('update_booking/', UpdateBookingView.as_view(), ),
+    path('delete_booking/', DeleteBookingView.as_view(), ),
 ]
