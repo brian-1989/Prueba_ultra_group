9cdaedeeb6fdb5cff986d9a8d85e4f76077d6e14
briayan <briayan.zapata@vivaair.com>
2023-03-06 19:52:11 -0500

    Added the Middleware

--- travel_agency/settings.py
+++ travel_agency/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'travel_agency.middleware.Middleware'
 ]
 
 ROOT_URLCONF = 'travel_agency.urls'
